9cac63cebb77f190e2d75fac10e0f124c7aed957
rana ramish <ramishrana687@gmail.com>
2025-01-28 12:31:04 +0500

    counter change

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,7 @@ import img2 from "./images/img2.png";
 import Block from "./Block/Block";
 import Featured from "./featured/Featured";
 import Subscriber from "./subscriber/subscriber";
-import Counter from "./counter/counter";
+import Counter from "./Counter/Counter";
 import Recent from "./recentblogs/Recent";
 import Maincard from "./maincard/Maincard";
 import { motion } from "framer-motion";
